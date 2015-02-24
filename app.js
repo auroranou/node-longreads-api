090ed97a0cc40df55882ac4e1f70d4541e82fb92
Aurora Nou <auroranou@gmail.com>
2015-02-24 17:32:03 -0500

    add docs, add public dir to app.js

--- app.js
+++ app.js
@@ -20,8 +20,32 @@ var schema = {
 
 var LongRead = mongoose.model('LongRead', schema, 'longreads');
 
-app.get('/longreads', function(req, res) {
-	LongRead.find({minuteLength: {$gt: 0, $lt: 15}}, function(err, doc) {
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.sendFile('index.html');
+})
+
+app.get('/longreads/short', function(req, res) {
+	LongRead.find({minuteLength: {$lt: 15}}, function(err, doc) {
+		res.send(doc);
+	});
+});
+
+app.get('/longreads/medium', function(req, res) {
+	LongRead.find({minuteLength: {$gt: 15, $lt: 30}}, function(err, doc) {
+		res.send(doc);
+	});
+});
+
+app.get('/longreads/long', function(req, res) {
+	LongRead.find({minuteLength: {$gt: 30, $lt: 45}}, function(err, doc) {
+		res.send(doc);
+	});
+});
+
+app.get('/longreads/longest', function(req, res) {
+	LongRead.find({minuteLength: {$gt:45}}, function(err, doc) {
 		res.send(doc);
 	});
 });
