0604762bba0eb3bf525244f8c8e8b4638b9a8998
Aurora Nou <auroranou@gmail.com>
2015-02-24 15:53:08 -0500

    testing queries

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var http = require('http');
+var mongoose = require('mongoose');
 
 var app = express();
 var router = express.Router();
 var server = http.createServer(app);
 
-app.get('/', function(req, res){
-  res.send('hello world');
+mongoose.connect('mongodb://localhost:27017/test');
+
+var schema = {
+	title:String,
+	articleUrl:String,
+	author:String,
+	source:String,
+	pubDate:String,
+	minuteLength:Number,
+	wordLength:String
+}
+
+var LongRead = mongoose.model('LongRead', schema, 'longreads');
+
+app.get('/longreads', function(req, res) {
+	LongRead.find({minuteLength: {$gt: 0, $lt: 15}}, function(err, doc) {
+		res.send(doc);
+	});
 });
 
-app.listen(3000);
+server.listen(3000);
