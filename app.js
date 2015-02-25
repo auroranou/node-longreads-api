0ee4febf1d92bf1b551ffc7cc576b7c7d93ee8c7
Aurora Nou <auroranou@gmail.com>
2015-02-25 10:31:54 -0500

    more fiddling with ports

--- app.js
+++ app.js
@@ -52,4 +52,6 @@ app.get('/longreads/longest', function(req, res) {
 	});
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
